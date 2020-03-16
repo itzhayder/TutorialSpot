9e7b2cb3e1a45eecbba7c6de7042e2d38c53bf4e
Maruf Ahmed <maruf_m1@yahoo.com>
2020-03-16 15:00:40 +0600

    fixed css missing link tags for playback

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/playback.css') }}" rel="stylesheet">
     @stack('styles')
 
 </head>
